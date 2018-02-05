38ae6fa20514686b4a802fc2a7ba303ca97c3e5b
kayd33 <kaydey287@gmail.com>
2018-02-05 20:35:35 +0530

    Fix typo in Issue #32 and used variables in user stylesheet

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,30 @@
-console.log('This would be the main JS file.');
+// console.log('This would be the main JS file.');
+$(document).ready(function () {
+  $('#main_content').headsmart()
+
+  var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.")
+  document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"))
+  try {
+    var pageTracker = _gat._getTracker("UA-130207-15");
+    pageTracker._trackPageview();
+  }
+  catch(err) {}
+})
+
+window.addEventListener("load", function(){
+  window.cookieconsent.initialise({
+    "palette": {
+      "popup": {
+        "background": "#000000",
+        "text": "#ffffff"
+      },
+      "button": {
+        "background": "transparent",
+        "text": "#ffffff",
+        "border": "#ffffff"
+      }
+    }
+  })
+})
+
+
